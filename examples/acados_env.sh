1e17f08f6f1afcda50666576381fa029a9c1328c
qq_28258903 <qq_28258903@noreply.gitcode.com>
2025-09-14 09:30:12 +0800

    add acados examples

---
+++ examples/acados_env.sh
@@ -0,0 +1,3 @@
+export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
+export ACADOS_SOURCE_DIR=/home/zhaoguodong/work/code/MAPPO-MPC-OmniDrones/acados/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/zhaoguodong/work/code/MAPPO-MPC-OmniDrones/acados/lib
